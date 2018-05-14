eb2c6991d1a6d78d98b3937e58d223769c04983d
witt <mymeek@163.com>
2018-05-14 12:18:46 +0800

    mybatis generator maven plugin

---
+++ mybatis-generator/src/main/java/com/witt/mg/dao/OrdersMapper.java
@@ -0,0 +1,30 @@
+package com.witt.mg.dao;
+
+import com.witt.mg.entity.Orders;
+import com.witt.mg.entity.OrdersExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface OrdersMapper {
+    int countByExample(OrdersExample example);
+
+    int deleteByExample(OrdersExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    List<Orders> selectByExample(OrdersExample example);
+
+    Orders selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByExample(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
---
+++ mybatis-generator/src/test/java/test/com/witt/mg/AppTestTest.java
@@ -0,0 +1,33 @@
+package test.com.witt.mg;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * AppTest Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>May 14, 2018</pre>
+ */
+public class AppTestTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: shouldAnswerWithTrue()
+     */
+    @Test
+    public void testShouldAnswerWithTrue() throws Exception {
+//TODO: Test goes here... 
+    }
+
+
+} 
